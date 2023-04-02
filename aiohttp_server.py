5409a48125dc7551f7cd0e1033f1b623a8ce479c
IvanDamNation <ivan.suvalkin@gmail.com>
2023-04-02 15:26:55 +0300

    Add frontend client file

--- aiohttp_server.py
+++ aiohttp_server.py
@@ -1,60 +1,58 @@
 import os
+
 from aiohttp import web
-from aiohttp.web import run_app
 
-WEBSOCKET_FILE = os.path.join(os.path.dirname(__file__), 'index.html')
-
+WS_FILE = os.path.join(os.path.dirname(__file__), "/index.html")
 
 routes = web.RouteTableDef()
 
 
 @routes.post('/news')
-async def post(request):
+async def post_news(request):
     if request.body_exists:
-        for websocket in request.application["sockets"]:
-            await websocket.send_str(await request.text())
+        for web_socket in request.application["sockets"]:
+            await web_socket.send_str(await request.text())
     return web.Response(status=200)
 
 
 @routes.get('/ws')
-async def handler(request):
+async def websocket_handler(request):
 
     response = web.WebSocketResponse(autoping=True, heartbeat=30)
     available = response.can_prepare(request)
-
     if not available:
-        with open(WEBSOCKET_FILE, 'rb') as ws_file:
-            return web.Response(body=ws_file.read(), content_type='text/html')
+        with open(WS_FILE, "rb") as ws_file:
+            return web.Response(body=ws_file.read(), content_type="text/html")
 
     await response.prepare(request)
 
     try:
-        request.app["sockets"].append(response)
+        request.application["sockets"].append(response)
 
-        async for message in response:
-            if message.type == web.WSMsgType.TEXT:
-                for web_socket in request.app['sockets']:
-                    if web_socket is not response:
-                        await web_socket.send_str(message.data)
+        async for msg in response:
+            if msg.type == web.WSMsgType.TEXT:
+                for ws in request.application["sockets"]:
+                    if ws is not response:
+                        await ws.send_str(msg.data)
             else:
                 return response
         return response
 
     finally:
-        request.app['sockets'].remove(response)
+        request.application["sockets"].remove(response)
 
 
-async def on_close(application: web.Application):
-    for web_socket in application['sockets']:
+async def on_shutdown(application: web.Application):
+    for web_socket in application["sockets"]:
         await web_socket.close()
 
 
 def init():
     application = web.Application()
-    application['sockets'] = []
+    application["sockets"] = []
     application.add_routes(routes)
-    application.on_close.append(on_close)
+    application.on_shutdown.append(on_shutdown)
     web.run_app(application)
 
 
-run_app()
+init()
