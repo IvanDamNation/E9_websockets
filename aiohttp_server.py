a4609afaa88c2419cc350551cb30854a4bd16172
IvanDamNation <ivan.suvalkin@gmail.com>
2023-04-02 14:40:16 +0300

    Refactor server part

--- aiohttp_server.py
+++ aiohttp_server.py
@@ -1,16 +1,25 @@
 import os
 from aiohttp import web
-import asyncio
-from asyncio import Queue
-import time
-import aiohttp
-
+from aiohttp.web import run_app
 
 WEBSOCKET_FILE = os.path.join(os.path.dirname(__file__), 'index.html')
 
 
-async def handler(request: web.Request):
-    response = web.WebSocketResponse(heartbeat=5)
+routes = web.RouteTableDef()
+
+
+@routes.post('/news')
+async def post(request):
+    if request.body_exists:
+        for websocket in request.application["sockets"]:
+            await websocket.send_str(await request.text())
+    return web.Response(status=200)
+
+
+@routes.get('/ws')
+async def handler(request):
+
+    response = web.WebSocketResponse(autoping=True, heartbeat=30)
     available = response.can_prepare(request)
 
     if not available:
@@ -18,32 +27,21 @@ async def handler(request: web.Request):
             return web.Response(body=ws_file.read(), content_type='text/html')
 
     await response.prepare(request)
-    await response.send_str('Hello!')
 
     try:
-        print('New joiner.')
-        for web_socket in request.app['sockets']:
-            await web_socket.send_str('New joiner.')
         request.app["sockets"].append(response)
 
         async for message in response:
             if message.type == web.WSMsgType.TEXT:
-                if message.data == "check":
-                    await response.send_str('connection OK')
-                else:
-                    for web_socket in request.app['sockets']:
-                        if web_socket is not response:
-                            await web_socket.send_str(message.data)
+                for web_socket in request.app['sockets']:
+                    if web_socket is not response:
+                        await web_socket.send_str(message.data)
             else:
                 return response
-
         return response
 
     finally:
         request.app['sockets'].remove(response)
-        print('Someone close connection.')
-        for web_socket in request.app['sockets']:
-            await web_socket.send_str('Someone close connection.')
 
 
 async def on_close(application: web.Application):
@@ -54,9 +52,9 @@ async def on_close(application: web.Application):
 def init():
     application = web.Application()
     application['sockets'] = []
-    application.router.add_get('/news', handler)
+    application.add_routes(routes)
     application.on_close.append(on_close)
-    return application
+    web.run_app(application)
 
 
-web.run_app(init())
+run_app()
